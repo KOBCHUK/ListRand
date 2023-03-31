98b467bc70e2c36c75c280484eec5ec7d8a6f196
Eugen Kobchuk <irravadi@gmail.com>
2023-03-31 20:30:08 +0300

    hm_ex_2 done

--- ListRand.py
+++ ListRand.py
@@ -1,1 +1,10 @@
+import random
+#numbers = [random.randint(-100,100) for _ in range (12)]
+numbers = [1,2,2,4,5,6,6,6,7,8]
+unique_numbers = []
+for n in numbers:
+    if n not in unique_numbers:
+        unique_numbers.append(n)
 
+print("Original list:", numbers)
+print("Unique list:", unique_numbers)
