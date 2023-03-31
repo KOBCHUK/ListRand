bf2f35c357dfbdb77d0f6dfa5d6c418cc5c70d20
Eugen Kobchuk <irravadi@gmail.com>
2023-03-31 20:13:50 +0300

    hm_ex_1 done

--- ListRand.py
+++ ListRand.py
@@ -1,1 +1,10 @@
+import random
 
+numbers = [random.randint(-100, 100) for _ in range(12)]
+
+even_count = len([n for n in numbers if n % 2 == 0])
+odd_count = len([n for n in numbers if n % 2 != 0])
+
+print("List of random integers:", numbers)
+print("Number of even numbers:", even_count)
+print("Number of odd numbers:", odd_count)
