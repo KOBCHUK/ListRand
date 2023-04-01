4cef60dd4139f3d2c0320f441fa796ba5f7279ff
Eugen Kobchuk <irravadi@gmail.com>
2023-04-01 11:02:46 +0300

    lab_ex_2 done

--- ListRand.py
+++ ListRand.py
@@ -1,16 +1,16 @@
 import random
 
-numbers = [random.randint(-100, 100) for _ in range(10)]
+start_list = [random.randint(-100, 100) for _ in range(10)]
 
-even_numbers = [n for n in numbers if n % 2 == 0]
+even_numbers = [n for n in start_list if n % 2 == 0]
 
-odd_numbers = [n for n in numbers if n % 2 != 0]
+odd_numbers = [n for n in start_list if n % 2 != 0]
 
-negative_numbers = [n for n in numbers if n < 0]
+negative_numbers = [n for n in start_list if n < 0]
 
-positive_numbers = [n for n in numbers if n > 0]
+positive_numbers = [n for n in start_list if n > 0]
 
-print("List of random integers:", numbers)
+print("List of random integers:", start_list)
 print("List of even numbers:", even_numbers)
 print("List of odd numbers:", odd_numbers)
 print("List of negative numbers:", negative_numbers)
