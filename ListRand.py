c53fb45a2d41c7ad9b874f69b52d9f2e81e8b517
Eugen Kobchuk <irravadi@gmail.com>
2023-04-01 09:58:37 +0300

    hm_ex_6 done

--- ListRand.py
+++ ListRand.py
@@ -1,1 +1,9 @@
+import random
 
+start_list = [random.randint(-100,100) for _ in range(12)]
+square_list =[]
+for n in start_list :
+    square_list.append(n ** 2)
+
+print(start_list)
+print(square_list)
