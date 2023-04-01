00af7b1bb628c0b60d53007e8b3b0e7a840ee768
Eugen Kobchuk <irravadi@gmail.com>
2023-04-01 10:06:37 +0300

    hm_ex_7 done

--- ListRand.py
+++ ListRand.py
@@ -1,1 +1,11 @@
+import random
 
+start_list = [random.randint(-100,100) for _ in range(12)]
+min_index = start_list.index(min(start_list))
+max_index = start_list.index(max(start_list))
+
+print(start_list)
+
+start_list[min_index], start_list[max_index] = start_list[max_index], start_list[min_index]
+
+print(start_list)
