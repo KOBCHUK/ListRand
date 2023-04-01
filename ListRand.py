4ee2c240a294fa87646a20a4799e4446ebd27286
Eugen Kobchuk <irravadi@gmail.com>
2023-04-01 10:15:31 +0300

    hm_ex_8 done

--- ListRand.py
+++ ListRand.py
@@ -1,1 +1,8 @@
+import random
+start_list = [random.randint(-100,100) for _ in range(12)]
+print(start_list)
+k = 5
+sorted_list = sorted(start_list, reverse=True)
+k_element = sorted_list[k - 1]
 
+print(k_element)
