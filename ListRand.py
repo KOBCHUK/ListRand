faa5569952b8c51e189e2777a342eacd26721d9e
Eugen Kobchuk <irravadi@gmail.com>
2023-04-01 10:31:03 +0300

    hm_ex_10 done

--- ListRand.py
+++ ListRand.py
@@ -1,1 +1,9 @@
+strings = ["sport", "crossfit", "jogging", "running", "box"]
+substring = "r"
+new_strings = []
 
+for string in strings:
+    if string.startswith(substring):
+        new_strings.append(string)
+print(strings)
+print(new_strings)
