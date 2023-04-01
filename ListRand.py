18700cd743679946b801cd0adcbd56f85694c9aa
Eugen Kobchuk <irravadi@gmail.com>
2023-04-01 10:24:19 +0300

    hm_ex_9 done

--- ListRand.py
+++ ListRand.py
@@ -1,1 +1,12 @@
+import random
+start_list = [random.randint(-100,100) for _ in range(12)]
+print(start_list)
+average = sum(start_list) / len(start_list)
+print(average)
+quantity = 0
 
+for n in start_list:
+    if n > average:
+        quantity += 1
+
+print(quantity)
