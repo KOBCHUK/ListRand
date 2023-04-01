0244199c1d36cfff92b3aa35c8c2e0af63d4cb72
Eugen Kobchuk <irravadi@gmail.com>
2023-04-01 09:30:11 +0300

    hm_ex-3 done

--- ListRand.py
+++ ListRand.py
@@ -1,4 +1,4 @@
-words = ["apple", "banana", "cherry", "date", "elderberry"]
+words = ["sport", "crossfit", "jogging", "running", "box"]
 sorted_words = sorted(words, key=len)
 print("Original list:", words)
 print("Sorted list:", sorted_words)
